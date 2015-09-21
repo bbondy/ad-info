e53f2dc04da957191ffc21f240965223337437a9
Brian R. Bondy <netzen@gmail.com>
2015-09-21 10:15:57 -0400

    Extract number of probably ad iframes from a url

--- src/test.js
+++ src/test.js
@@ -2,13 +2,41 @@ var page = require('webpage').create(),
     system = require('system'),
     t, url;
 
-function onPageReady() {
+function checkReadyState(done) {
+  setTimeout(function () {
+    var readyState = page.evaluate(function () {
+      return document.readyState;
+    });
+
+    if ("complete" === readyState) {
+      done();
+    } else {
+      checkReadyState(done);
+    }
+  }, 15000);
+}
+
+function pageLoadDone() {
   var htmlContent = page.evaluate(function () {
-    return document.querySelectorAll('iframe').length;
-    // return document.documentElement.outerHTML;
+    function isSupportedAdSize(width, height) {
+      return width === 728 && height === 90 ||
+        width === 300 && height === 250 ||
+        width === 160 && height === 600 ||
+        width === 320 && height === 50;
+    }
+
+    var iframes = document.querySelectorAll('iframe');
+    var iframeSrcs = [];
+    for (var i = 0; i < iframes.length; i++) {
+      var iframe = iframes[i];
+      if (isSupportedAdSize(iframe.clientWidth, iframe.clientHeight)) {
+        iframeSrcs.push(iframe.src);
+      }
+    }
+    return iframeSrcs;
   });
 
-  console.log(htmlContent);
+  console.log(htmlContent.length);
   phantom.exit();
 }
 
@@ -19,21 +47,6 @@ if (system.args.length === 1) {
   t = Date.now();
   url = system.args[1];
   page.open(url, function (status) {
-    function checkReadyState() {
-      setTimeout(function () {
-        var readyState = page.evaluate(function () {
-          return document.readyState;
-        });
-
-        if ("complete" === readyState) {
-          onPageReady();
-        } else {
-          checkReadyState();
-        }
-      });
-    }
-    checkReadyState();
+    checkReadyState(pageLoadDone);
   });
 }
-
-
