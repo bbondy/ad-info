0bebf78526cee6cb897f15f76d9a51ca6d535879
Brian R. Bondy <netzen@gmail.com>
2015-09-21 10:50:05 -0400

    Add stack trace and more iframe detail

--- src/test.js
+++ src/test.js
@@ -1,6 +1,5 @@
 var page = require('webpage').create(),
-    system = require('system'),
-    t, url;
+    system = require('system');
 
 function checkReadyState(done) {
   setTimeout(function () {
@@ -17,7 +16,7 @@ function checkReadyState(done) {
 }
 
 function pageLoadDone() {
-  var htmlContent = page.evaluate(function () {
+  var iframesData = page.evaluate(function () {
     function isSupportedAdSize(width, height) {
       return width === 728 && height === 90 ||
         width === 300 && height === 250 ||
@@ -26,27 +25,55 @@ function pageLoadDone() {
     }
 
     var iframes = document.querySelectorAll('iframe');
-    var iframeSrcs = [];
+    var iframesData = [];
     for (var i = 0; i < iframes.length; i++) {
       var iframe = iframes[i];
-      if (isSupportedAdSize(iframe.clientWidth, iframe.clientHeight)) {
-        iframeSrcs.push(iframe.src);
+      var rect = iframe.getBoundingClientRect();
+      if (isSupportedAdSize(rect.width, rect.height)) {
+        iframesData.push({
+          src: iframe.src,
+          width: rect.width,
+          height: rect.height,
+          top: rect.top,
+          left: rect.left
+        });
       }
     }
-    return iframeSrcs;
+    return iframesData;
   });
 
-  console.log(htmlContent.length);
+  for (var i = 0; i < iframesData.length; i++) {
+    var iframeData = iframesData[i];
+    console.log(JSON.stringify(iframeData));
+  }
   phantom.exit();
 }
 
 if (system.args.length === 1) {
-    console.log('Usage: test.js <some URL>');
-    phantom.exit(1);
-} else {
-  t = Date.now();
-  url = system.args[1];
+  console.log('Usage: test.js <some URL>');
+  phantom.exit(1);
+}
+
+
+function main() {
+  var url = system.args[1];
+  page.onConsoleMessage = function(msg, lineNum, sourceId) {
+    console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+  };
+  page.onError = function(msg, trace) {
+    var msgStack = ['ERROR: ' + msg];
+    if (trace && trace.length) {
+      msgStack.push('TRACE:');
+      trace.forEach(function(t) {
+        msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+      });
+    }
+    console.error(msgStack.join('\n'));
+  };
+
   page.open(url, function (status) {
     checkReadyState(pageLoadDone);
   });
 }
+
+main();
