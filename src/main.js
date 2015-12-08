beac261b4301fb91b421116ee6d2a7ec6229e58d
Brian R. Bondy <netzen@gmail.com>
2015-12-07 23:01:25 -0500

    Add MPL-2.0 header on source file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // Note: The process.env.PAGE_TIMEOUT environment variable may be set to override
 // the default number of milliseconds this process will wait for all page
 // requests to complete.
